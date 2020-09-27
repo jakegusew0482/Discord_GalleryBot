3acc13591dda78afd9e28c81b7c59a47a187c55d
timnub <tanewton@buffalo.edu>
2020-09-27 14:12:16 -0400

    Added file downloading shell

--- bot.py
+++ bot.py
@@ -6,8 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+CHANNEL_NAME = "gallery-submissions"
+ALLOWED_FILES = [".png", ".jpg", ".gif"]
+DOWNLOAD_DIR = "img"
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
@@ -16,8 +20,9 @@ async def on_ready():
             break
 
         print(f'{client.user} is connected to:\n'
-             f'{guild.name}, ID: {guild.id}'
-          )
+              f'{guild.name}, ID: {guild.id}'
+              )
+
 
 @client.event
 async def on_message(message):
@@ -28,8 +33,19 @@ async def on_message(message):
 
     if message.content == 'howdy':
         await message.channel.send(quote)
+
+    if message.channel.name == CHANNEL_NAME:
+        for attachment in message.attachments:
+            extension = attachment.filename[-4:]
+            if extension not in ALLOWED_FILES:
+                continue
+
+            url = attachment.url
+            download_file(DOWNLOAD_DIR, url)
+
+
+def download_file(directory, url):
+    pass
+
+
 client.run(TOKEN)
-
-
-
-
